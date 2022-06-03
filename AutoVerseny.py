e440fea4e9e8265af7a72662555e6d8096f8b8d8
Englert Ervin  Brunó <englert@users.noreply.github.com>
2022-06-03 06:33:31 +0200

    Update AutoVerseny.py

--- AutoVerseny.py
+++ AutoVerseny.py
@@ -1,18 +1,19 @@
-#csapat;       versenyzo;    eletkor; palya;             korido;   kor
-#   0               1           2        3                 4        5
-#Versenylovak; Fürge Ferenc; 29;      Gran Prix Circuit; 00:01:11; 1
-
+'''https://infojegyzet.hu/vizsgafeladatok/okj-programozas/rendszeruzemelteto-koridok/
+autoverseny.csv:
+csapat;versenyzo;eletkor;palya;korido;kor
+Versenylovak;Fürge Ferenc;29;Gran Prix Circuit;00:01:11;1
+'''
 class AutóVerseny:
     def __init__( self, sor ):
-        s                 = sor.strip().split( ';' )
-        self.csapat       =      s[0]
-        self.versenyzo    =      s[1]
-        self.eletkor      = int( s[2] )
-        self.palya        =      s[3]
-        self.korido_split =[ int(i)  for i in s[4].split( ':' ) ]
-        self.korido       =      s[4]
-        self.korido_sec   = 3600 * int( s[4][:2] ) + 60 * int( s[4][3:5] ) + int( s[4][6:] ) 
-        self.kor          = int( s[5] )
+        csapat,versenyzo, eletkor, palya, korido, kor = sor.strip().split( ';' )
+        self.csapat       = csapat  
+        self.versenyzo    = versenyzo 
+        self.eletkor      = int(eletkor)
+        self.palya        = palya 
+        self.korido       = korido
+        self.korido_split = [ int(i)  for i in korido.split( ':' ) ]
+        self.korido_sec   = 3600 * int( korido[:2] ) + 60 * int( korido[3:5] ) + int( korido[6:] ) 
+        self.kor          = int(kor)
         
 with open( 'autoverseny.csv', 'r', encoding='utf-8-sig' ) as f:
     fejléc = f.readline()
